c09eb52329be8023a1fbb82025a25f1774a456a1
ahmadgunawanmadania <ahmadgunawan@madania.sch.id>
2026-08-11 10:45:31 +0700

    Refactor registration handler import in update function

--- api/handleUpdateRegistration.js
+++ api/handleUpdateRegistration.js
@@ -1,4 +1,4 @@
-const { handler } = require('../lib/handler');
+
 const { handleUpdateRegistration } = require('../lib/registration');
 
 module.exports = handler((formObj, docId) => handleUpdateRegistration(formObj, docId));
