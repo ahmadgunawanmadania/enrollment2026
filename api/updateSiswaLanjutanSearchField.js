c38741b2a98dda0dec5b46af9d6ffbf7ddd8268f
ahmadgunawanmadania <ahmadgunawan@madania.sch.id>
2026-08-11 10:42:21 +0700

    Update updateSiswaLanjutanSearchField.js

--- api/updateSiswaLanjutanSearchField.js
+++ api/updateSiswaLanjutanSearchField.js
@@ -1,4 +1,4 @@
-const { handler } = require('../lib/handler');
+
 const { updateSiswaLanjutanSearchField } = require('../lib/registration');
 
 module.exports = handler((pageToken) => updateSiswaLanjutanSearchField(pageToken));
