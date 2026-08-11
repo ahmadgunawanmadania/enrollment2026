728cc84b688a342033fb1ab5bc39e62087be686c
ahmadgunawanmadania <ahmadgunawan@madania.sch.id>
2026-08-11 10:39:32 +0700

    Remove maxDuration config from bulkUploadSiswaLanjutan
    Remove maxDuration configuration from bulk upload module.

--- api/bulkUploadSiswaLanjutan.js
+++ api/bulkUploadSiswaLanjutan.js
@@ -2,4 +2,3 @@ const { handler } = require('../lib/handler');
 const { bulkUploadSiswaLanjutan } = require('../lib/registration');
 
 module.exports = handler(() => bulkUploadSiswaLanjutan());
-module.exports.config = { maxDuration: 60 };
