5970d5d5baef207dda6e7051e5aa1f06ac2c0e87
ahmadgunawanmadania <ahmadgunawan@madania.sch.id>
2026-08-11 10:42:59 +0700

    Update updatePendaftaranSearchField.js

--- api/updatePendaftaranSearchField.js
+++ api/updatePendaftaranSearchField.js
@@ -1,4 +1,4 @@
-const { handler } = require('../lib/handler');
+
 const { updatePendaftaranSearchField } = require('../lib/registration');
 
 module.exports = handler((pageToken) => updatePendaftaranSearchField(pageToken));
