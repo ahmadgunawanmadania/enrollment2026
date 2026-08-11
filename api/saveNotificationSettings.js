39a57c5610df1501fc8526e31609c14217b3dc1a
ahmadgunawanmadania <ahmadgunawan@madania.sch.id>
2026-08-11 10:44:03 +0700

    Update saveNotificationSettings.js

--- api/saveNotificationSettings.js
+++ api/saveNotificationSettings.js
@@ -1,4 +1,4 @@
-const { handler } = require('../lib/handler');
+
 const { saveNotificationSettings } = require('../lib/settings');
 
 module.exports = handler((settings) => saveNotificationSettings(settings));
