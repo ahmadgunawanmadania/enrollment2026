5d71e8836e40dc90b830188c2d04de418787373f
ahmadgunawanmadania <ahmadgunawan@madania.sch.id>
2026-08-12 08:21:03 +0700

    Refactor handleUpdateRegistration to use handler

--- api/handleUpdateRegistration.js
+++ api/handleUpdateRegistration.js
@@ -1,5 +1,4 @@
-
+const { handler } = require('../lib/handler');
 const { handleUpdateRegistration } = require('../lib/registration');
 
 module.exports = handler((formObj, docId) => handleUpdateRegistration(formObj, docId));
-module.exports.config = { maxDuration: 60 };
